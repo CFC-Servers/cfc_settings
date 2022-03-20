7360cdeb19063d1b99f12659962504ae684dd722
legokidlogan <legokidlogan@gmail.com>
2022-03-20 15:06:53 -0600

    Remove panel flashing

--- lua/cfc_settings/client/cl_config.lua
+++ lua/cfc_settings/client/cl_config.lua
@@ -1,42 +1,3 @@
-local isValid = IsValid
-local mFloor = math.floor
-local mMax = math.max
-
-local COLOR_RED = Color( 255, 0, 0, 255 )
-
--- Utility functions
-local function flashPanel( panel, noRepeats, length, blinkLength, thickness )
-    if not isValid( panel ) or not ispanel( panel ) then return end
-    if noRepeats and panel.cfcSettings_hasFlashed then return end
-
-    panel.cfcSettings_hasFlashed = true
-    length = mMax( length or 2, 0.01 )
-    blinkLength = mMax( blinkLength or 0.2, 0.01 )
-    thickness = thickness or 5
-
-    local blinkState = true
-    local panelStr = tostring( panel )
-
-    timer.Create( "CFC_Settings_FlashPanel_" .. panelStr, blinkLength, mFloor( length / blinkLength ), function()
-        blinkState = not blinkState
-    end )
-
-    timer.Create( "CFC_Settings_FlashPanel_RemoveHook_" .. panelStr, length, 1, function()
-        hook.Remove( "PostRenderVGUI", "CFC_Settings_FlashPanel_" .. panelStr )
-    end )
-
-    hook.Add( "PostRenderVGUI", "CFC_Settings_FlashPanel_" .. panelStr, function()
-        if not blinkState then return end
-
-        local x, y = panel:LocalToScreen()
-        local w, h = panel:GetSize()
-
-        surface.SetDrawColor( COLOR_RED )
-        surface.DrawOutlinedRect( x, y, w, h, thickness )
-    end )
-end
-
-
 -- Main formatting table
 local settingsTable = {
     {
@@ -69,13 +30,11 @@ local settingsTable = {
                 leftfunc = function()
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "Falco's prop protection buddies", true )
-                    flashPanel( controlpanel.Get( "Falco's prop protection buddies" ), true )
                 end,
                 rightfunc = function( settingsMenu )
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "Falco's prop protection buddies", true )
                     settingsMenu:SetVisible( false )
-                    flashPanel( controlpanel.Get( "Falco's prop protection buddies" ), true )
                 end,
             } },
         }
@@ -121,13 +80,11 @@ local settingsTable = {
                 leftfunc = function()
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "custom_hitmarkers", true )
-                    flashPanel( controlpanel.Get( "custom_hitmarkers" ), true )
                 end,
                 rightfunc = function( settingsMenu )
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "custom_hitmarkers", true )
                     settingsMenu:SetVisible( false )
-                    flashPanel( controlpanel.Get( "custom_hitmarkers" ), true )
                 end,
             } },
             { custom_propinfo_enabled = { type = "bool", displayName = "Enable prop info" } },
@@ -142,13 +99,11 @@ local settingsTable = {
                 leftfunc = function()
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "custom_propinfo", true )
-                    flashPanel( controlpanel.Get( "custom_propinfo" ), true )
                 end,
                 rightfunc = function( settingsMenu )
                     LocalPlayer():ConCommand( "+menu" )
                     spawnmenu.ActivateTool( "custom_propinfo", true )
                     settingsMenu:SetVisible( false )
-                    flashPanel( controlpanel.Get( "custom_propinfo" ), true )
                 end,
             } },
             { physgun_buildmode_enabled = { type = "bool", displayName = "Enable physgun buildmode" } },
